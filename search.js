42b763b10d181726aea3a731f7e5f643de6be28c
lintool <jimmylin@umd.edu>
2014-03-03 21:41:56 -0500

    Refactoring.

--- search.js
+++ search.js
@@ -5,14 +5,10 @@ var searcher = (function () {
 
   var startTime;
 
-  function search_init(callback) {
-    startTime = new Date();
-    dfs = {};
-
+  function getDfs() {
     for (var i=0; i<queryTerms.length; i++) {
-      var transaction = db.transaction(["df"],"readonly");
-      var store = transaction.objectStore("df");
-      var request = store.get(queryTerms[i]);
+      var request = db.transaction(["df"], "readonly")
+                      .objectStore("df").get(queryTerms[i]);
  
       request.onerror = function(e) {
         console.log("Error", e.target.error.name);
@@ -20,91 +16,76 @@ var searcher = (function () {
 
       request.onsuccess = (function(query) { return function(e) {
         dfs[query] = e.target.result;
-        //console.log(query + ", df=" + e.target.result);
       }})(queryTerms[i]);
     }
+  }
+
+  function searchInit(callback) {
+    startTime = new Date();
+    dfs = {};
 
     // Note there's a race condition here: we're depending on the stat
-    // requests completing before we actual perform query evaluation...
+    // requests completing before we actual perform query evaluation.
+    getDfs();
 
-    var query = queryTerms[0];
-    //console.log("query term[0]: " + query);
-    var transaction = db.transaction(["postings"], "readonly");
-    var store = transaction.objectStore("postings");
+    // We're going to open a cursor on the first query term now.
+    var qterm = queryTerms[0];
+    var cursor = db.transaction(["postings"], "readonly").objectStore("postings")
+                   .openCursor(IDBKeyRange.bound(qterm + sep, qterm + "."));
 
-    var startTime = new Date().getTime();
+    var results = {}; // The accumulator
 
-    //console.log("Start time: " + startTime);
-
-    var range = IDBKeyRange.bound(query + sep, query + ".");
-    var cursor = store.openCursor(range);
-
-    var cnt = 0;
-
-    var results = {};
     cursor.onsuccess = function(e) {
       var res = e.target.result;
       if (res) {
         var parts = res.key.split(sep);
+        var pterm = parts[0];
+        var pdoc = parts[1];
       }
 
-      if ( res && parts[0] == query ) {
-        // tweets.length is hardcoded
-        results[parts[1]] = res.value * Math.log(numDocs/dfs[parts[0]]);;
-        cnt++;
+      if ( res && pterm == qterm ) {
+        results[pdoc] = res.value * Math.log(numDocs/dfs[pterm]);;
         res.continue();
       }
 
-      if ( !res || parts[0] != query ) {
-        var end = new Date().getTime();
-        //console.log(Object.keys(results).length + " hits in " + (end-startTime) + "ms");
-        //console.log(results);
-
+      if ( !res || pterm != qterm ) {
         if (queryTerms.length > 1 ) {
-          search_cont(1, results, callback);
+          // Query has more than one term: continue evaluating other terms.
+          searchContinuation(1, results, callback);
         } else {
+          // One term query: we're done.
           callback(results, startTime);
         }
       }
     };
   }
 
-  function search_cont(n, results, callback) {
-    var query = queryTerms[n];
-    //console.log("query term[" + n + "]: " + query);
-
-    var transaction = db.transaction(["postings"], "readonly");
-    var store = transaction.objectStore("postings");
-
-    var startTime = new Date().getTime();
-
-    var range = IDBKeyRange.bound(query + sep, query + ".");
-    var cursor = store.openCursor(range);
-
-    var cnt = 0;
+  function searchContinuation(n, results, callback) {
+    var qterm = queryTerms[n];
+    var cursor = db.transaction(["postings"], "readonly").objectStore("postings")
+                   .openCursor(IDBKeyRange.bound(qterm + sep, qterm + "."));
 
     cursor.onsuccess = function(e) {
       var res = e.target.result;
       if (res) {
         var parts = res.key.split(sep);
+        var pterm = parts[0];
+        var pdoc = parts[1];
       }
 
-      if ( res && parts[0] == query ) {
-        if ( parts[1] in results) {
-          // tweets.length is hardcoded
-          results[parts[1]] += res.value * Math.log(numDocs/dfs[parts[0]]);
+      if ( res && pterm == qterm ) {
+        if ( pdoc in results) {
+          results[pdoc] += res.value * Math.log(numDocs/dfs[pterm]);
         }
-
-        cnt++;
         res.continue();
       }
 
-      if ( !res || parts[0] != query ) {
-        var end = new Date().getTime();
-        //console.log((end-startTime) + "ms");
+      if ( !res || pterm != qterm ) {
         if ( n < queryTerms.length-1 ) {
-          search_cont(n+1, results, callback);
+          // More terms to process...
+          searchContinuation(n+1, results, callback);
         } else {
+          // We're done!
           callback(results, startTime);
         }
       }
@@ -114,7 +95,7 @@ var searcher = (function () {
   return {
     search : function(qt, callback) {
       queryTerms = qt;
-      search_init(callback);
+      searchInit(callback);
     },
 
     setNumDocs : function(n) {
